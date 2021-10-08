b45af4fbd7282e1597bb6fd324f24ebc07a78bf2
nouf1999 <70525803+nouf1999@users.noreply.github.com>
2021-10-08 15:17:33 +0000

    splash is ok

--- lib/main.dart
+++ lib/main.dart
@@ -11,82 +11,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// Normal Logo Splash screen
-    Widget example1 = SplashScreenView(
-      navigateRoute: const Homepage(),
-      duration: 3000,
-      imageSize: 130,
-      imageSrc: "icon.png",
-      backgroundColor: Colors.white,
-    );
-/* 
     /// Logo with animated Colorize text
-    Widget example2 = SplashScreenView(
-      navigateRoute: SecondScreen(),
+    // ignore: non_constant_identifier_names
+    Widget Splash = SplashScreenView(
+      navigateRoute: const Homepage(),
       duration: 5000,
-      imageSize: 130,
-      imageSrc: "splashscreen_image.png",
-      text: "Splash Screen",
+      imageSize: 150,
+      imageSrc: "images/icon.png",
+      text: "iContact",
       textType: TextType.ColorizeAnimationText,
-      textStyle: TextStyle(
+      textStyle: const TextStyle(
         fontSize: 40.0,
       ),
-      colors: [
+      colors: const [
         Colors.purple,
         Colors.blue,
         Colors.yellow,
         Colors.red,
       ],
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blue,
     );
 
-    /// Logo with Typer Animated Text example
-    Widget example3 = SplashScreenView(
-      navigateRoute: SecondScreen(),
-      duration: 3000,
-      imageSize: 130,
-      pageRouteTransition: PageRouteTransition.Normal,
-      imageSrc: "splashscreen_image.png",
-      speed: 100,
-      text: "Splash Screen",
-      textType: TextType.TyperAnimatedText,
-      textStyle: TextStyle(
-        fontSize: 30.0,
-      ),
-      backgroundColor: Colors.white,
-    );
-
-    /// Logo with Scale Animated Text example
-    Widget example4 = SplashScreenView(
-      navigateRoute: SecondScreen(),
-      duration: 3000,
-      imageSize: 130,
-      imageSrc: "splashscreen_image.png",
-      text: "Splash Screen",
-      textType: TextType.ScaleAnimatedText,
-      textStyle: TextStyle(
-        fontSize: 30.0,
-      ),
-      backgroundColor: Colors.white,
-    );
-
-    /// Logo with Normal Text example
-    Widget example5 = SplashScreenView(
-      navigateRoute: SecondScreen(),
-      duration: 3000,
-      imageSize: 130,
-      imageSrc: "splashscreen_image.png",
-      text: "Splash Screen",
-      textType: TextType.NormalText,
-      textStyle: TextStyle(
-        fontSize: 30.0,
-      ),
-      backgroundColor: Colors.white,
-    );
- */
     return MaterialApp(
-      title: 'Splash screen Demo',
-      home: example1,
+      title: 'iContact',
+      home: Splash,
     );
   }
 }
